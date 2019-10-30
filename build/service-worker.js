9ebbc5445b1ac1548837f1bb78fe0facaab91a2e
shimonbe <shimonbe@checkpoint.com>
2019-10-30 21:02:34 +0200

    initial

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.365ad048518a0cf5afa45c31aa667741.js"
+  "/precache-manifest.e35e7f16f3caaa27b419190d6920a0c9.js"
 );
 
 self.addEventListener('message', (event) => {
